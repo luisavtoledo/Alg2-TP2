6cb9b2b808cccb2d46b03cf44ae2011d50d6cb64
Luisa Toledo <luisavtoledo@gmail.com>
2023-06-29 13:14:31 -0300

    Commit inicial

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import numpy as np
+
+#Distancia de Minkowski
+def dist_Minkowski(X, Y, p):
+    somatorio =np.sum(np.power(np.abs(np.array(X) - np.array(Y)), p))
+    return np.power(somatorio, 1/p)
+
+#Matriz de distancia
+
+#Algoritmo 2-aproximado para o problema dos k-centros
