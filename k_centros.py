23bcaef9be739d5df9874861c1f2917e82c16579
Luisa Toledo <luisavtoledo@gmail.com>
2023-07-04 18:16:59 -0300

    Alteracoes

---
+++ k_centros.py
@@ -0,0 +1,74 @@
+import numpy as np
+import math
+
+#Calcula a distancia de Minkowski
+def dist_Minkowski(X, Y, p):
+    somatorio = np.sum(np.power(np.abs(np.array(X) - np.array(Y))), p)
+    return np.power(somatorio, 1/p)
+
+#Computa a matriz de distancia
+def matriz_dist(S, p):
+    t = S.shape[0]
+    matriz = np.zeros((t, t))
+    for i in range(t):
+        for j in range(t):
+            matriz[i][j] = dist_Minkowski(S[i], S[j], p)
+    return matriz
+
+#Acha o maior raio entre um ponto e um centro
+def maior_distancia(matriz_dist, i_centros):
+    ponto_distante = 0
+    maior_raio = 0
+    t = matriz_dist.shape[0]
+
+    for i in range(t):
+        distancia = math.inf
+    
+        #Encontra a distancia entre o ponto e o seu centro
+        for j in range(len(i_centros)):
+            if matriz_dist[i][i_centros[j]] < distancia:
+                distancia = matriz_dist[i][i_centros[j]]
+
+        if distancia > maior_raio:
+            #Substitui o maior raio até agora pela distancia encontrada
+            maior_raio = distancia
+            ponto_distante = i
+
+    return ponto_distante
+
+#Algoritmo 2-aproximado para o problema dos k-centros
+def k_centros(matriz_dist, k):
+
+    t = matriz_dist.shape[0]
+        
+    #Cria o conjunto com indices da solucao e adiciona um ponto arbitrario
+    i_centros = np.array([])
+    primeiro_centro = np.random.randint(0, t)
+    i_centros.append(primeiro_centro)
+
+    #Adiciona recursivamente o ponto mais distante dos centros
+    for i in range (k-1):
+        novo_centro = maior_distancia(matriz_dist, i_centros)
+        i_centros.append(novo_centro)
+
+    return i_centros
+
+#Retorna os k-centros
+def centros(S, k, p):
+
+    #Se k for menor ou igual a quantidade de pontos, retorna o dataset
+    t = S.shape[0] 
+    if k >= t:
+        return S
+    
+    else:
+        distancias = matriz_dist(S, p)
+        i_centros = k_centros(distancias, k)
+
+        #Cria o conjunto solucao 
+        centros = np.array([])
+        for i in range(len(i_centros)):
+            indice = i_centros[i]
+            centros.append(S[indice])
+
+    return centros
