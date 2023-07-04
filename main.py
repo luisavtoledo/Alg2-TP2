d4040dcbcd60d0f2f1011603ac8d56441612f653
Luisa Toledo <luisavtoledo@gmail.com>
2023-07-04 19:34:05 -0300

    ajustes

--- main.py
+++ main.py
@@ -7,10 +7,28 @@ from sklearn.metrics import silhouette_score, adjusted_rand_score
 import time
 
 def testes(dataset, p):
+    dados = dataset.iloc[:, 0:-1]
+    classes = dataset.iloc[:, [-1]]
+    k = classes.nunique()
+    dados = dados.to_numpy
+
+    #Armazenando metricas
+    tempo_kcentros = []
+    raio_kcentros = []
+    silhouette_kcentros = []
+    rand_kcentros = []
+    tempo_kmeans = []
+    raio_kmeans = []
+    silhouette_kmeans = []
+    rand_kmeans = []
+
+    matriz_dist = matriz_dist(dados, p)
+
     
+
 
 def main(dataset):
     print("p = 1")
     testes(dataset, 1)
-    print("p=2")
+    print("p = 2")
     testes(dataset, 2)
